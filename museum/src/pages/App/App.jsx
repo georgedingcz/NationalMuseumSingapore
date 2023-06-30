e02c01f1a0157e5c5ed6af4b0cebb226e8e407c8
georgeding <dingchangzhao@hotmail.com>
2023-06-30 11:29:31 +0800

    add nav

--- museum/src/pages/App/App.jsx
+++ museum/src/pages/App/App.jsx
@@ -1,10 +1,26 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import NewOrderPage from "../NewOrderPage/NewOrderPage";
 import AuthPage from "../AuthPage/AuthPage";
 import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
+import NavBar from "../../components/NavBar/NavBar";
 
 export default function App() {
-  const [user, setUser] = useState(null);
-  return <main className="App">{user ? <NewOrderPage /> : <AuthPage />}</main>;
+  const [user, setUser] = useState({});
+  return (
+    <main className="App">
+      {user ? (
+        <>
+          <NavBar />
+          <Routes>
+            <Route path="/orders" element={<OrderHistoryPage />} />
+            <Route path="/orders/new" element={<NewOrderPage />} />
+          </Routes>
+        </>
+      ) : (
+        <AuthPage />
+      )}
+    </main>
+  );
 }
