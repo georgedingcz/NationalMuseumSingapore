f83a70716263465b23088e8786813eeb2bd22971
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-06-30 14:13:41 +0800

    Change Ticketing.jsx

--- museum/src/pages/TicketingPage/TicketingPage.jsx
+++ museum/src/pages/TicketingPage/TicketingPage.jsx
@@ -1,10 +1,21 @@
-import TicketingButton from "../../components/TicketingButton/TicketingButton";
+import "./TicketingPage.css";
 
 export default function TicketingPage() {
   return (
     <>
-      <p>Ticketing Page</p>
-      <TicketingButton />
+      <div className="page-container">
+        <p>Ticketing</p>
+        <div>
+          <div className="progress">progress tracker thing</div>
+        </div>
+        <div className="check-buttons">
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+          <input type="text" />
+        </div>
+        <button>Confirm Selection</button>
+      </div>
     </>
   );
 }
