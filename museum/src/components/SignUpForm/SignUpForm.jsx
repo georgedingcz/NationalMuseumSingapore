fb0439c3501d4fc7e56bb024e4589eb8e22c25aa
georgeding <dingchangzhao@hotmail.com>
2023-06-30 11:59:35 +0800

    create users-api and users-service

--- museum/src/components/SignUpForm/SignUpForm.jsx
+++ museum/src/components/SignUpForm/SignUpForm.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { signUp } from "../../utilities/users-service";
 
 export default class SignUpForm extends Component {
   state = {
@@ -17,9 +18,25 @@ export default class SignUpForm extends Component {
     });
   };
 
-  handleSubmit = (evt) => {
+  handleSubmit = async (evt) => {
+    // Prevent form from being submitted to the server
     evt.preventDefault();
-    alert(JSON.stringify(this.state));
+    try {
+      // We don't want to send the 'error' or 'confirm' property,
+      //  so let's make a copy of the state object, then delete them
+      const formData = { ...this.state };
+      delete formData.error;
+      delete formData.confirm;
+      // The promise returned by the signUp service method
+      // will resolve to the user object included in the
+      // payload of the JSON Web Token (JWT)
+      const user = await signUp(formData);
+      // Baby step!
+      console.log(user);
+    } catch {
+      // An error occurred
+      this.setState({ error: "Sign Up Failed - Try Again" });
+    }
   };
 
   render() {
