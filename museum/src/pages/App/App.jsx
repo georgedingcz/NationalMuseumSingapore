a9e54e7943dc9bf41e0e35c47e5b58a651947d4a
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-07-01 23:34:07 +0800

    Update Navbar, Routes
    The nav will now show your name on login.
    
    The app routing has been fixed to show the homepage after logged in

--- museum/src/pages/App/App.jsx
+++ museum/src/pages/App/App.jsx
@@ -12,42 +12,34 @@ import MapPage from "../MapPage/MapPage";
 import ItineraryPage from "../ItineraryPage/ItineraryPage";
 import Footer from "../../components/Footer/Footer";
 import ExhibitionDetails from "../Exhibition/ExhibitionDetails";
+import { Navigate } from "react-router-dom";
 
 export default function App() {
   const [user, setUser] = useState(getUser());
   return (
     <main className="App">
       <NavBar user={user} setUser={setUser} />
-
-      {user ? (
-        <>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/map" element={<MapPage />} />
-            <Route path="/exhibition" element={<Exhibition />} />
-            <Route path="/exhibition/:id" element={<ExhibitionDetails />} />
-            <Route path="/ticketing" element={<TicketingPage />} />
-            <Route path="/check-out" element={<CheckOutPage />} />
+      <>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/exhibition" element={<Exhibition />} />
+          <Route path="/exhibition/:id" element={<ExhibitionDetails />} />
+          <Route path="/ticketing" element={<TicketingPage />} />
+          <Route path="/check-out" element={<CheckOutPage />} />
+          {user ? (
             <Route path="/itinerary" element={<ItineraryPage />} />
-          </Routes>
-        </>
-      ) : (
-        <>
-          <Routes>
+          ) : (
             <Route
               path="/auth"
               element={<AuthPage setUser={setUser} user={user} />}
             />
-            <Route path="/" element={<HomePage />} />
-            <Route path="/map" element={<MapPage />} />
-            <Route path="/exhibition" element={<Exhibition />} />
-            <Route path="/exhibition/:id" element={<ExhibitionDetails />} />
-            <Route path="/ticketing" element={<TicketingPage />} />
-            <Route path="/check-out" element={<CheckOutPage />} />
-            <Route path="/itinerary" element={<AuthPage setUser={setUser} />} />
-          </Routes>
-        </>
-      )}
+          )}
+          {/* Redirect to the default page */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </>
+
       <Footer />
     </main>
   );
