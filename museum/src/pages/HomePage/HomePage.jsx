dffff3fbdb23868b8cef8f967430ebec4d5eec98
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-07-01 14:00:17 +0800

    Update Bug fix

--- museum/src/pages/HomePage/HomePage.jsx
+++ museum/src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,9 @@
-import ExhibitionCard from "../../components/Card/ExhibitionCard";
 import MuseumCard from "../../components/Museum/MuseumCard";
 import ItineraryButton from "../../components/Itinerary/ItineraryButton";
 import MapButton from "../../components/MapButton/MapButton";
 import TicketingButton from "../../components/TicketingButton/TicketingButton";
 import Exhibition from "../Exhibition/Exhibition";
 import "../HomePage/HomePage.css";
-import ItineraryPage from "../ItineraryPage/ItineraryPage";
 
 export default function HomePage() {
   return (
