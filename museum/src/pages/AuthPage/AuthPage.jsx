2718636934cad8e1b830651773c71e14fd9614f8
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-07-01 23:44:06 +0800

    Update AuthPage layout, css
    Hi

--- museum/src/pages/AuthPage/AuthPage.jsx
+++ museum/src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import SignUpForm from "../../components/SignUpForm/SignUpForm";
+import "./authPage.css";
 
-export default function AuthPage({setUser}) {
+export default function AuthPage({ setUser }) {
+  const [showLoginForm, setShowLoginForm] = useState(false);
+
+  const toggleForm = () => {
+    setShowLoginForm(!showLoginForm);
+  };
+
   return (
     <main>
-      <h1>AuthPage</h1>
-      <SignUpForm setUser={setUser} />
-      <LoginForm setUser={setUser} />
+      <div className="page-container">
+        <div className="login-signup-form">
+          <h1>Welcome!</h1>
+          <p>Login or Sign Up to continue</p>
+          {showLoginForm ? (
+            <>
+              <LoginForm setUser={setUser} />
+              <p>
+                Don't have an account?{" "}
+                <button onClick={toggleForm}>Sign Up</button>
+              </p>
+            </>
+          ) : (
+            <>
+              <SignUpForm setUser={setUser} />
+              <p>Already have an account?</p>
+              <button onClick={toggleForm}>Log In</button>
+            </>
+          )}
+        </div>
+      </div>
     </main>
   );
 }
