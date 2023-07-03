33a414acb11db0fc8f2afd8a93e3d37e2ef318ab
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-07-03 14:51:12 +0800

    Updated Tour, TourPage.jsx

---
+++ src/components/Tour/Tour.jsx
@@ -0,0 +1,7 @@
+export default function Tour() {
+  return (
+    <div className="section-container">
+      <p>This is the tour component</p>
+    </div>
+  );
+}
---
+++ src/pages/Tour/TourPage.jsx
@@ -0,0 +1,10 @@
+import Tour from "../../components/Tour/Tour";
+
+export default function TourPage() {
+  return (
+    <div className="page-container">
+      <h1>This is the TourPage</h1>
+      <Tour />
+    </div>
+  );
+}
