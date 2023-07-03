a1617f7545899c83eb7743c6a91ca239180e7e2c
haruthepotato <timothysoonyufeng@gmail.com>
2023-07-03 11:04:00 +0800

    Update CardImage.jsx

--- museum/src/components/Card/CardImage.jsx
+++ museum/src/components/Card/CardImage.jsx
@@ -1,7 +1,7 @@
 export default function CardImage(props) {
   return (
     <>
-      <div className="image-container"><img alt={props.img} src={props.img}></img></div>
+      <div className="image-container"><img alt={props.image} src={props.image}></img></div>
     </>
   );
 }
