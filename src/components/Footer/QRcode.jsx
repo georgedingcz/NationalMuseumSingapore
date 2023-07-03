bffd254434d95047cd576ab48b8b4a825cff07a1
georgeding <dingchangzhao@hotmail.com>
2023-07-04 02:26:55 +0800

    contact us page; minor css changes for qrcode and awards icons

--- src/components/Footer/QRcode.jsx
+++ src/components/Footer/QRcode.jsx
@@ -3,11 +3,11 @@ export default function QRcode() {
     <>
       <img
         src="https://www.nhb.gov.sg/nationalmuseum/-/media/nms2017/image/visit-us/httpsgogovsgnmsvouch.png?h=125&w=137"
-        alt="QR code" className="footerIcon"
+        alt="QR code" className="qrCode"
       />
       <img
         src="https://www.nhb.gov.sg/nationalmuseum/-/media/nms2017/image/visit-us/mobile-with-dome-bot.png?h=138&w=82"
-        alt="QR code guide" className="footerIcon"
+        alt="QR code guide" className="qrCode"
       />
     </>
   );
