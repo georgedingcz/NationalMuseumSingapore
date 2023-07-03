a008060fa2fa4b00cc70a14974956080064f78cc
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-07-03 15:07:27 +0800

    Update Card.css

--- src/components/Tour/Tour.jsx
+++ src/components/Tour/Tour.jsx
@@ -1,7 +1,20 @@
+import Card from "../Card/Card";
+
 export default function Tour() {
+  const card = {
+    image: "https://picsum.photos/200",
+    date: "2023-07-01",
+    title: "Tour Title",
+    content: {
+      description: "This is the content of the card.",
+    },
+    additional_notes: "Additional notes for the card.",
+  };
+
   return (
     <div className="section-container">
       <p>This is the tour component</p>
+      <Card card={card} />
     </div>
   );
 }
