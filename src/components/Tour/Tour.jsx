8cfd2bc9f2cb1d05edeafc574a681250464f7b4e
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-07-04 10:44:11 +0800

    Tour jsx

--- src/components/Tour/Tour.jsx
+++ src/components/Tour/Tour.jsx
@@ -1,26 +1,16 @@
-import Card from "../Card/Card";
 import TourCard from "./TourCard";
 
 export default function Tour() {
-  const card = {
-    image: "https://picsum.photos/200",
-    date: {
-      start_date: "",
-      end_date: "",
-      permanent: true,
-    },
-    title: "Tour Title",
-    content: {
-      description: "This is the content of the card.",
-    },
-    additional_notes: "Additional notes for the card.",
-  };
-
   return (
     <>
-      <div className="section-container"></div>
-
       <div className="section-container">
+        <div className="disclaimer-box">
+          The group size of each tour is limited to a maximum of 15 pax{" "}
+          (excluding guide). Note: You are encouraged to contact our front desk
+          on the tour schedule and availabilit, as the tours are subjected to
+          changes. Each tour is approximately one hour, on a first come, first
+          served basis.
+        </div>
         <TourCard />
       </div>
     </>
--- src/components/Tour/TourCard.jsx
+++ src/components/Tour/TourCard.jsx
@@ -2,14 +2,14 @@ import "./Tour.css";
 
 export default function TourCard() {
   return (
-    <div>
-      <div className="tourCard">
-        <img
-          className="tourCard_img"
-          src="https://picsum.photos/200"
-          alt="tour-card-img"
-        />
-        <p>This is a tour card</p>
+    <div className="tourCard">
+      <img
+        className="tourCard_img"
+        src="https://picsum.photos/500/400"
+        alt="tour-card-img"
+      />
+      <div className="tourCard-header">
+        <h3>Singapore History Gallery</h3>
       </div>
     </div>
   );
