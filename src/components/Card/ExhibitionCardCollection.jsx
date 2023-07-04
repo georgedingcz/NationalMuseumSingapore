b959012e4285b79bc9e13a6e3d20861fbee2ef78
haruthepotato <timothysoonyufeng@gmail.com>
2023-07-04 09:49:16 +0800

    adjusted exhibition card collection

--- src/components/Card/ExhibitionCardCollection.jsx
+++ src/components/Card/ExhibitionCardCollection.jsx
@@ -2,27 +2,11 @@ import { Link } from "react-router-dom";
 import Card from "./Card";
 import React, { useEffect, useState } from 'react';
 
-export default function ExhibitionCardCollection() {
-
-    const [exhibitions, setExhibitions] = useState([]);
-
-    useEffect(() => {
-      const fetchExhibitions = async () => {
-        try {
-          const response = await fetch('/exhibition');
-          const data = await response.json();
-          setExhibitions(data);
-        } catch (error) {
-          console.error(error);
-        }
-      };
-      fetchExhibitions();
-    }, []);
-
+export default function ExhibitionCardCollection(props) {
     return (
         <>  
             <div className="row">
-                {exhibitions.map((card) => (
+                {props.elements.map((card) => (
                         <Link className="link" to={card.id}>
                             <Card card={card} />
                         </Link>
