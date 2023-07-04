acc959310d1c087cc1147962cbc90e6c9086e7dd
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-07-04 15:45:14 +0800

    Update ticketing page

---
+++ src/components/TicketingButton/CheckoutPage1.jsx
@@ -0,0 +1,27 @@
+export default function CheckoutPage() {
+  return (
+    <div className="section-container">
+      <div className="checkout">
+        <div className="selection-box">
+          <label>ALL ACCESS</label>
+          <input type="checkbox" />
+        </div>
+        <div className="selection-box">
+          <label>PERMANENT GALLERIES</label>
+          <input type="checkbox" />
+        </div>
+        <div className="selection-box">
+          <label>Semangat yang baru</label>
+          <input type="checkbox" />
+        </div>
+        <div className="selection-box">
+          <label>Now Boarding</label>
+          <input type="checkbox" />
+        </div>
+        <div>
+          <button>Confirm Selection</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/TicketingPage/TicketingPage.jsx
+++ src/pages/TicketingPage/TicketingPage.jsx
@@ -1,37 +1,16 @@
-import SiteButton from "../../components/general/SiteButton";
+import CheckoutPage from "../../components/TicketingButton/CheckoutPage1";
 import "./TicketingPage.css";
 
 export default function TicketingPage() {
   return (
     <>
       <div className="page-container">
-        <p>Ticketing</p>
-        <div>
+        <div className="section-container">
+          <p>Ticketing</p>
           <div className="progress">progress tracker thing</div>
         </div>
-        <div>
-          <div className="checkout">
-            <div className="selection-box">
-              <label>ALL ACCESS</label>
-              <input type="checkbox" />
-            </div>
-            <div className="selection-box">
-              <label>PERMANENT GALLERIES</label>
-              <input type="checkbox" />
-            </div>
-            <div className="selection-box">
-              <label>Semangat yang baru</label>
-              <input type="checkbox" />
-            </div>
-            <div className="selection-box">
-              <label>Now Boarding</label>
-              <input type="checkbox" />
-            </div>
-            <button>Confirm Selection</button>
-          </div>
-        </div>
+        <CheckoutPage />
       </div>
-      <SiteButton />
     </>
   );
 }
