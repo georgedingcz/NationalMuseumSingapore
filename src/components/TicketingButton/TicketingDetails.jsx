f34198cc85b3297cc0b06c825a4b1588454c3c68
Khairul-Ikhwan <khairul.ikhwan00@gmail.com>
2023-07-06 00:06:09 +0800

    Update ticketing

--- src/components/TicketingButton/TicketingDetails.jsx
+++ src/components/TicketingButton/TicketingDetails.jsx
@@ -1,3 +1,24 @@
 export default function TicketingDetails() {
-  return <h1>Ticketing deets</h1>;
+  return (
+    <>
+      <div className="section-container">
+        <section className="details-container">
+          <h3>Nationality</h3>
+          <button>Singaporeans & PR</button>
+          <button>Tourists & Foreign Residents</button>
+        </section>
+        <section className="details-container">
+          <h3>Ticket Type</h3>
+          <button>Adult</button>
+          <button>Senior</button>
+          <button>Student</button>
+          <button>Child</button>
+        </section>
+        <section className="details-container">
+          <input type="text" disabled value="Ticket Price" />
+          <input type="text" disabled />
+        </section>
+      </div>
+    </>
+  );
 }
