a8366a5eaf1944f289cdec571dcc517ae8f5ae76
timsyf <timothysoonyufeng@gmail.com>
2023-07-07 01:25:15 +0800

    add loading to homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,15 +8,19 @@ import React, { useEffect, useState } from 'react';
 export default function HomePage() {
   const [happenings, setHappenings] = useState([]);
   const [exhibitions, setExhibitions] = useState([]);
+  const [loading, setLoading] = useState(false); // Added loading state
 
   useEffect(() => {
     const fetchHappenings = async () => {
       try {
+        setLoading(true); // Set loading state to true
         const response = await fetch('/exhibition/search?status=current');
         const data = await response.json();
         setHappenings(data);
+        setLoading(false); // Set loading state to false after fetching data
       } catch (error) {
         console.error(error);
+        setLoading(false); // Set loading state to false in case of error
       }
     };
     fetchHappenings();
@@ -25,11 +29,14 @@ export default function HomePage() {
   useEffect(() => {
     const fetchExhibitions = async () => {
       try {
+        setLoading(true); // Set loading state to true
         const response = await fetch('/exhibition');
         const data = await response.json();
         setExhibitions(data);
+        setLoading(false); // Set loading state to false after fetching data
       } catch (error) {
         console.error(error);
+        setLoading(false); // Set loading state to false in case of error
       }
     };
     fetchExhibitions();
@@ -38,29 +45,43 @@ export default function HomePage() {
   return (
     <div className="page-container">
       <div className="section-container">
-        <MuseumCard />
-        <TicketingButton />
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            <MuseumCard />
+            <TicketingButton />
+          </>
+        )}
       </div>
 
       <div className="section-container no-flex card-collection-fix">
         <h2>Happening Now</h2>
         <div className="card-collection-fix">
-          <CardCollectionSlider data={happenings} />
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <CardCollectionSlider data={happenings} />
+          )}
         </div>
       </div>
-        <Link to="/itinerary">
-          <button>ITINERARY PLANNER</button>
-        </Link>
+      <Link to="/itinerary">
+        <button>ITINERARY PLANNER</button>
+      </Link>
 
       <div className="section-container no-flex card-collection-fix">
         <h2>Exhibitions</h2>
         <div className="card-collection-fix">
-          <CardCollectionSlider data={exhibitions} />
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <CardCollectionSlider data={exhibitions} />
+          )}
         </div>
       </div>
-        <Link to="/map">
-          <button>MUSEUM MAP</button>
-        </Link>
+      <Link to="/map">
+        <button>MUSEUM MAP</button>
+      </Link>
     </div>
   );
 }
